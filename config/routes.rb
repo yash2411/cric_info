e85d7b93c265fd481109135ef8028c763c473c21
Yash <yashpandey1106@gmail.com>
2024-03-06 13:34:31 +0530

    initial commit and bootstrap configuration

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+
+
+# 1756971e-4973-413e-b588-331299491ff2
